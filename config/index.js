337a530c340d0cb6b603c1c5a7494a080e8a7d2f
hopstee <hopsteee@gmail.com>
2021-04-18 22:04:00 +0300

    update host name

--- config/index.js
+++ config/index.js
@@ -3,4 +3,4 @@ const DEV = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT;
 
 
-export const server = DEV ? `http://localhost:${PORT}` : 'https://your_deployment.server.com';
+export const server = DEV ? `http://localhost:${PORT}` : 'https://edkr.site';
