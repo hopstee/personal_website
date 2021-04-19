be4016feeb32f248773adc821d72b6b2c6dad969
hopstee <47725106+hopstee@users.noreply.github.com>
2021-04-19 10:28:25 +0300

    Update mail.js

--- pages/mail.js
+++ pages/mail.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Input from '../components/input/Input'
 
 const Mail = () => {
@@ -59,43 +60,54 @@ const Mail = () => {
     }
 
     return (
-        <div className="w-full h-full md:h-screen">
-            <div className="flex items-center w-full h-full">
-                <div className="w-full p-3 sm:p-6 md:p-0">
-                    <div className="relative bg-white md:w-2/3 xl:w-1/3 rounded-xl mx-auto">
-                        <div className="card">
-                            <div className="card-body">
-                                <form onSubmit={submitForm} className="p-2">
-                                    <div className="w-full text-center mt-4 uppercase text-lg font-semibold main-text-color">
-                                        Email me
-                                    </div>
-                                    <div>
-                                        <Input 
-                                            title="Name" 
-                                            placeholder="Ivan Ivanov"
-                                        />
-                                        <Input 
-                                            title="Email" 
-                                            placeholder="your@email.com"
-                                        />
-                                        <Input 
-                                            title="Message" 
-                                            placeholder="I would like..."
-                                            textarea={true} 
-                                        />
-                                    </div>
-                                    <div className="mt-8 flex justify-end">
-                                        <button type="submit" id="email_submit_btn" className="px-4 py-2 w-full md:w-auto main-btn-color rounded-lg focus:outline-none text-white transition-all duration-100">
-                                            Send
-                                        </button>
-                                    </div>
-                                </form>
+        <>
+            
+            <Head>
+                <title>Resume</title>
+                <meta name="keywords" content="HTML, CSS, JavaScript, Send email"></meta>
+                <meta name="description" content="Contact me if you would like to work with me, say Hi, or ask a question."></meta>
+                <meta name="author" content="Eduard Krivovyashchuk"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="w-full h-full md:h-screen">
+                <div className="flex items-center w-full h-full">
+                    <div className="w-full p-3 sm:p-6 md:p-0">
+                        <div className="relative bg-white md:w-2/3 xl:w-1/3 rounded-xl mx-auto">
+                            <div className="card">
+                                <div className="card-body">
+                                    <form onSubmit={submitForm} className="p-2">
+                                        <div className="w-full text-center mt-4 uppercase text-lg font-semibold main-text-color">
+                                            Email me
+                                        </div>
+                                        <div>
+                                            <Input 
+                                                title="Name" 
+                                                placeholder="Ivan Ivanov"
+                                            />
+                                            <Input 
+                                                title="Email" 
+                                                placeholder="your@email.com"
+                                            />
+                                            <Input 
+                                                title="Message" 
+                                                placeholder="I would like..."
+                                                textarea={true} 
+                                            />
+                                        </div>
+                                        <div className="mt-8 flex justify-end">
+                                            <button type="submit" id="email_submit_btn" className="px-4 py-2 w-full md:w-auto main-btn-color rounded-lg focus:outline-none text-white transition-all duration-100">
+                                                Send
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
