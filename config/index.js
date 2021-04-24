f5f5879113a57c01faf3e69ec8441d760b3241c2
hopstee <hopsteee@gmail.com>
2021-04-24 21:56:24 +0300

    add page loader bar

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const DEV = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT;
+const RESURSE = process.env.RESURSE;
 
 
-export const server = DEV ? `http://localhost:${PORT}` : 'https://edkr.site';
+export const server = DEV ? `http://localhost:${PORT}` : RESURSE;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-// import 'tailwindcss/tailwind.css'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
+
 import '../styles/globals.css'
 
 import Layout from '../components/Layout'
+
+const progress = new ProgressBar({
+    size: 2,
+    className: "bg-blue-500",
+    delay: 100
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
     return (
